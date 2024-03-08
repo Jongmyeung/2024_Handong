6f1bff8568903762c692a7ce8ef25864aea17972
Jongmyeung <danieljml7love@gmail.com>
2024-03-08 22:29:52 +0900

    test : test

--- Data_Structure/HW/Homeworkd1.cpp
+++ Data_Structure/HW/Homeworkd1.cpp
@@ -1,6 +1,23 @@
-#include <iostream?
+#include <iostream>
 
-int main(){
+using namespace std;
 
-    cout << "hello" << endl;
+
+int main() {
+    int num1, num2;
+
+    // 사용자로부터 두 개의 정수를 입력받음
+    cout << "Enter the first number: ";
+    cin >> num1;
+
+    cout << "Enter the second number: ";
+    cin >> num2;
+
+    // 두 수를 더함
+    int sum = num1 + num2;
+
+    // 결과를 출력
+    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << std::endl;
+
+    return 0;
 }
---
+++ Java_Programming/HW/Homework1.java
@@ -0,0 +1,11 @@
+public class Homework1{
+    public static void main(String[] args) {
+        Homework1 hw1 = new Homework1();
+
+        hw1.run();
+    }
+
+    public void run(){
+        System.out.println("hello pangil");
+    }
+}  
