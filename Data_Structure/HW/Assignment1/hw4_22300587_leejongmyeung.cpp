c71e0f6e5ea1fbdb1cb2bda9572566b4f2d3a0e8
Jongmyeung <danieljml7love@gmail.com>
2024-03-22 19:49:19 +0900

    Fix : fix the range

--- Data_Structure/HW/Assignment1/hw4_22300587_leejongmyeung.cpp
+++ Data_Structure/HW/Assignment1/hw4_22300587_leejongmyeung.cpp
@@ -3,72 +3,34 @@
 using namespace std;
 
 #define SIZE 100
+#define YES 1
+#define NO 0
 
-class palin_stack{
-    char s[SIZE];
-    int top;
-    public:
-        palin_stack();
-        void push(char x);
-        char pop();
-};
-
-palin_stack::palin_stack()
+int check(string input, int a, int b) // a, b는 index의 처음과 마지막을 의미 -> palindrome이니까!
 {
-    top = 0;
+    if(b - a < 1)
+        return 1;
+    else {
+        if(input[a] == input[b])
+            return 1 * check(input, a+1, b-1);
+        else
+            return 0 * check(input, a+1, b-1); // 만약 같지 않으면 0을 곱한다. 그렇게 하면 결과값이 0이 나오게 되어 있다.
+    }
 }
 
-void palin_stack::push(char x)
-{
-    s[top] = x;
-    top++;
-}
-
-char palin_stack::pop()
-{
-    top--;
-    return(s[top]);
-}
-
-int is_palin(char x1, char x2)
-{
-    if(x1 == x2)
-        return 1; // 같으면
-    else 
-        return 2; // 다르면
-}
 
 int main() {
 
-    string input, output;
-    palin_stack stack1;
-    cout << "Input string : ";
+    string input;
+    int len;
+    cout << "Input a string : ";
     cin >> input;
 
-    // input.size()의 값의 상관없이 2로 나누면 된다. 왜냐하면 2로 나누면 몫만 남기 때문에 홀수여도 가운데 문자를 무시할 수 있다.
-    // 이거는 입력 때만 같다.
-    int check = 0;
-    int ha_len = (input.size() / 2);
-    for(int i = 0; i < ha_len; i++){
-        stack1.push(input[i]);
-    }
-    if(input.size() % 2 == 0){ // 짝수일 때 좀 다름...
-        for(int i = ha_len; i < input.size(); i++){
-            check = is_palin(stack1.pop(), input[i]);
-            if(check == 2)
-                break;
-        }
-    } else {
-        for(int i = ha_len + 1; i < input.size(); i++){
-            check = is_palin(stack1.pop(), input[i]);
-            if(check == 2)
-                break;
-        }
-    }
-    if(check == 2)
-        output += "No, It’s not a palindrome.\n";
-    if(check == 1)
-        output += "Yes, it’s a palindrome.\n";
+    len = input.size();
+    if(check(input, 0, len-1) == YES)
+        cout << "Yes, it is a palindrome. \n";
+    else 
+        cout << "No, it is not a palindrome. \n";
 
-    cout << output;
+    return 0;
 }
